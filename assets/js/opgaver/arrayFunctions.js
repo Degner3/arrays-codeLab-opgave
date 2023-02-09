40c0886e5df0479fe6b9b1d5ec81074cd3420390
Degner3 <niklaschristiansen@live.dk>
2023-02-09 12:59:21 +0100

    S

--- assets/js/opgaver/arrayFunctions.js
+++ assets/js/opgaver/arrayFunctions.js
@@ -9,6 +9,11 @@ let myPartArray = ['partElement1', 'partElement2', 'PartElement3'];
 // opgave 1 tilføj element4 til enden myArray
 
 // din kode her...
+
+console.log(myArray);
+
+myArray.push('element4');
+
 console.log(myArray);
 
 // opgave 1 fjern element4 igen
@@ -16,16 +21,30 @@ console.log(myArray);
 // din kode her...
 console.log(myArray);
 
+myArray.pop();
+
+console.log(myArray);
+
 //.........................................................
 
 // opgave 2 tilføj element4 til starten myArray
 
 // din kode her...
+
+console.log(myArray);
+
+myArray.unshift('element4');
+
 console.log(myArray);
 
 // opgave 2 fjern element4 igen
 
 // din kode her...
+
+console.log(myArray);
+
+myArray.shift();
+
 console.log(myArray);
 
 //.........................................................
@@ -36,9 +55,11 @@ let myVegetables = ['salat', 'blomkål', 'spidskål', 'agurk'];
 
 /* opgave 3 splice()
  tilføj mandarin og fersken efter pære i myFruits
- */
+*/
 
 // din kode her...
+
+console.log(myFruits);
 
 myFruits.splice(2, 0, "mandarin", "fersken");
 
@@ -47,6 +68,9 @@ console.log(myFruits);
 // opgave 3 fjern mandarin og fersken igen
 
 // din kode her...
+
+console.log(myFruits);
+
 myFruits.splice(2, 2);
 
 console.log(myFruits);
@@ -59,6 +83,9 @@ og ind i en ny array der hedder myCabbage*/
 
 // din kode her...
 
+console.log(myVegetables);
+
+let myCabbage = myVegetables.slice(1, 3)
 
 
 console.log(myCabbage);
@@ -66,11 +93,12 @@ console.log(myCabbage);
 //.........................................................
 /* opgave 5 concat()
  sæt arrays myFruits og myVegetables sammen i en ny array der hedder myYummies
- */
+*/
 
 
 // din kode her...
 
+let myYummies = myFruits.concat(myVegetables);
 
 
 console.log(myYummies);
@@ -84,16 +112,24 @@ console.log(myYummies);
 
 // din kode her...
 
+console.log(myYummies);
 
+myYummies.sort();
 
 console.log(myYummies);
 
+// console.log(myYummies);
+
 /* opgave 6 reverse()
  vend rækkefølgen på  array  myYummies fra opgave 6
- */
+*/
 
 
 // din kode her...
+
+console.log(myYummies);
+
+myYummies.reverse();
 
 console.log(myYummies);
 
@@ -103,11 +139,24 @@ console.log(myYummies);
  brug map funktionen på myNumbers, og skab en ny array, der hedder myNextNumbers hvor alle tal er 7 større end 
  tallene i myNumbers. brug igen map funktionen på din nye array, myNextNumbers. og skab en ny array
   der hedder myLastNumbers der indeholder tal der er 12 gange så store som tallene i myNextNumbers.
- */
+*/
 let myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // din kode her...
+
+console.log(myNumbers);
+
+    let myNextNumbers = myNumbers.map((x) =>{
+    return x * 7;
+});
+
 console.log(myNextNumbers);
+
+let myLastNumbers = myNextNumbers.map((x) =>{
+
+    return x * 12;
+});
+
 console.log(myLastNumbers);
 
 //.........................................................
@@ -115,6 +164,19 @@ console.log(myLastNumbers);
 skriv en kode der kan give os en ny array, udfra myNames, hvor navnet keld ikke findes i.
 */
 let myNames = ['keld', 'søren', 'jens', 'tine', 'keld', 'ib', 'lene', 'keld', 'frederikke', 'line', 'kurt'];
+
+console.log(myNames);
+
+const result = myNames.filter(nameCheck);
+
+function nameCheck(myNames) {
+
+    if(myNames != 'keld'){
+    return myNames;
+    }
+}
+
+console.log(result);
 
 
 //.........................................................
--- assets/js/opgaver/examples.js
+++ assets/js/opgaver/examples.js
@@ -9,16 +9,33 @@ myExtrasInput.addEventListener("change", addExtra);
 
 function addExtra() {
     /* din map kode her... */
+
+    myExtras.push(myExtrasInput.value);
+    
+    // console.log(myExtras);
+
+    updateExtras();
+    
+
 }
 
 
-
 function updateExtras() {
+    /* din map kode her... */
 
-
-
+    let mytext = '<strong>Du har valgt</strong><br><ul> ' ;
+    // console.log('extras array: ' + myExtras);
 
     /* din map kode her... */
+    myExtras.map((items)=> {
+
+        mytext = mytext + '<li>' + items + '</li>';
+
+    });
+
+    mytext = mytext + '</ul>';
+
+    myExtrasListElement.innerHTML = mytext
 
 
 }
